75b11c415719bc8e01069ef155c4e725f699d157
mohammadariful20 <arifulz848@gmail.com>
2024-12-08 01:37:05 +0600

    set navber

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,9 +3,9 @@ import Navbar from "./share/Navbar";
 
 const Home = () => {
     return (
-        <div className="bg-[#07332F] w-full lg:w-11/12 mx-auto">
-            <Navbar></Navbar>
-        </div>
+        <section className="w-full lg:w-10/12 mx-auto">
+            <Navbar />
+        </section>
     );
 };
 
--- src/pages/home/share/Navbar.jsx
+++ src/pages/home/share/Navbar.jsx
@@ -1,22 +1,18 @@
-
+import Group1 from '../../../assets/icons/Group 1.svg'
 const Navbar = () => {
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-[#07332F] text-white">
             <div className="flex-1">
-                <a className="btn btn-ghost text-xl">Doc House</a>
+                <img src={Group1} alt="Doc House icon" />
+                <a className="btn btn-ghost text-xl"><span className='text-[#F7A582]'>Doc</span> House</a>
             </div>
             <div className="flex-none">
                 <ul className="menu menu-horizontal px-1">
-                    <li><a>Link</a></li>
-                    <li>
-                        <details>
-                            <summary>Parent</summary>
-                            <ul className="bg-base-100 rounded-t-none p-2">
-                                <li><a>Link 1</a></li>
-                                <li><a>Link 2</a></li>
-                            </ul>
-                        </details>
-                    </li>
+                    
+                    <li><a>Home</a></li>
+                    <li><a>About</a></li>
+                    <li><a>Appointment</a></li>
+                    <li><a>Login</a></li>
                 </ul>
             </div>
         </div>
